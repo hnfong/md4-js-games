a65d91facfe74e631099ea5efa2c1b580c144653
cmliu <cmliu@f3f8e9c3-4f37-0410-9a2b-95bf28f32e7a>
2007-08-16 11:06:54 +0000

    minor bug fix: room's name = 0 treated as false
    git-svn-id: http://lonelyisland.svn.sourceforge.net/svnroot/lonelyisland/trunk@51 f3f8e9c3-4f37-0410-9a2b-95bf28f32e7a

--- rooms.inc.php
+++ rooms.inc.php
@@ -4,7 +4,7 @@ function get_room_names()
  $base = "rooms";
  $d = opendir($base);
  $ret = array();
- while ($l = readdir($d)) {
+ while (false !== ($l = readdir($d))) {
   if (substr($l,0,1) == '.') continue;
   $f = $base . "/" . $l;
   if (is_file($f))
